8039dbf6dce6196ee53c6a93647aa35e61e22f62
qjpstorrecampo <qjpstorrecamp@tip.edu.ph>
2023-10-12 04:09:01 +0800

    pages added

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Products from "../admin/pages/Products"
+
+const LandingPage = () => {
+  return (
+    <div className='text-3xl font-bold underline'>
+      <div>
+        <Products/>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
